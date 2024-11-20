c1699d41eeb4ee3c4af9b826a06e74ec0e0af044
Nkosikhona Mlaba <nkosimlaba397@gmail.com>
2024-11-20 04:41:32 +0200

    added functionality to track number of users and increase the known faces if an additional user is detected

--- src/main/python/za/co/theemlaba/face_recognition_service.py
+++ src/main/python/za/co/theemlaba/face_recognition_service.py
@@ -1,9 +1,16 @@
 import numpy as np
 import cv2
 import face_recognition
-from database import fetch_user_photos
+from database import fetch_user_photos, get_user_count
+import time
+
+known_encodings = []
+user_ids = []
+initial_user_count = 0
 
 def load_faces_from_db():
+    global known_encodings, user_ids, initial_user_count
+    
     records = fetch_user_photos()
 
     known_encodings = []
@@ -14,9 +21,16 @@ def load_faces_from_db():
         encoding = face_recognition.face_encodings(img)[0]
         known_encodings.append(encoding)
         user_ids.append(user_id)
+    
+    initial_user_count = len(user_ids)
     return known_encodings, user_ids
 
-def recognize_face(frame, known_encodings, user_ids):
+def recognize_face(frame, encodings, ids):
+    global known_encodings, user_ids, initial_user_count
+
+    if known_encodings == [] or user_ids == []:
+        return None
+    
     faces_cur_frame = face_recognition.face_locations(frame)
     encodes_cur_frame = face_recognition.face_encodings(frame, faces_cur_frame)
 
@@ -28,3 +42,39 @@ def recognize_face(frame, known_encodings, user_ids):
         if matches[match_index]:
             return user_ids[match_index]
     return None
+
+def load_new_faces_from_db():
+    """Append new faces and user IDs from the database."""
+    global known_encodings, user_ids, initial_user_count
+    
+    records = fetch_user_photos()
+    
+    # new faces if any
+    new_encodings = []
+    new_user_ids = []
+    
+    for user_id, photo_blob in records:
+        if user_id not in user_ids:
+            photo = np.frombuffer(photo_blob, np.uint8)
+            img = cv2.imdecode(photo, cv2.IMREAD_COLOR)
+            encoding = face_recognition.face_encodings(img)[0]
+            new_encodings.append(encoding)
+            new_user_ids.append(user_id)
+    
+    known_encodings.extend(new_encodings)
+    user_ids.extend(new_user_ids)
+    
+    initial_user_count = len(user_ids)
+    return known_encodings, user_ids
+
+def update_faces_if_new_user():
+    """Checks for new users every 60 seconds and updates face recognition data."""
+    global initial_user_count, known_encodings, user_ids
+    while True:
+        time.sleep(10)
+        current_user_count = get_user_count()
+        
+        if current_user_count > initial_user_count:
+            load_new_faces_from_db()
+            initial_user_count = current_user_count
+            print("New user added to facial recognition system.")
